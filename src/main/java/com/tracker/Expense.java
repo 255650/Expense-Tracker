0a935cb945c6106d77389d6dfec508a0fab73144
Kacper Kazimierczak <255622@edu.p.lodz.pl>
2026-06-20 18:25:06 +0200

    refaktor expense

--- src/main/java/com/tracker/Expense.java
+++ src/main/java/com/tracker/Expense.java
@@ -6,6 +6,6 @@ public class Expense extends Transaction {
                    String category,
                    LocalDate date,
                    String description) {
-        super(-amount, category, date, description);
+        super(amount*-1, category, date, description);
     }
 }
